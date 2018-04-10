f970d41c28716d34da18ec1f2a7b86d3b8cbf2d9
Tuan Cao <antuantc@gmail.com>
2018-04-10 14:52:53 -0500

    Add penny invalid test

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -2,6 +2,8 @@
 {
     public enum Coin
     {
+        None = 0,
+        Penny = 1,
         Nickle = 5,
         Dime = 10,
         Quarter = 25
@@ -15,7 +17,14 @@
 
         public bool InsertCoin(Coin coin)
         {
-            return true;
+            if (coin.Equals(Coin.Penny))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
