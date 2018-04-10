5d058e3618927513e4a30a7dd35cfa722728eaac
Tuan Cao <antuantc@gmail.com>
2018-04-10 14:45:31 -0500

    First test

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,21 @@
+﻿namespace VendingMachines
+{
+    public enum Coin
+    {
+        Nickle = 5,
+        Dime = 10,
+        Quarter = 25
+    }
+
+    public class VendingMachine
+    {
+        public VendingMachine()
+        {
+        }
+
+        public bool InsertCoin(Coin coin)
+        {
+            return true;
+        }
+    }
+}
