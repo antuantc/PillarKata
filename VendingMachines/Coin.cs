f3c9ec018ba6dcc5602c5167b015f28b680aed28
Tuan Cao <antuantc@gmail.com>
2018-04-27 10:57:54 -0500

    Reconfigure coins, use memberdata for xunit tests, remove CONST in const names.

---
+++ VendingMachines/Coin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachines
+{
+    public class Coin
+    {
+        private double _weight;  //in grams
+        private double _size;  //diameter in mm
+
+        public Coin()
+        {
+            //Default to penny
+            _weight = 2.5;
+            _size = 19.05;
+        }
+
+        public Coin(double weight, double size)
+        {
+            _weight = weight;
+            _size = size;
+        }
+
+        public decimal coinValue
+        {
+            get
+            {
+                //Check size and weight with an error of +-.05
+                if (_weight >= 2.45 && _weight <= 2.55
+                    && _size >= 19 && _size <= 19.1)
+                {
+                    return .01m;
+                }
+                else if (_weight >= 4.95 && _weight <= 5.05
+                        && _size >= 21.16 && _size <= 21.26)
+                {
+                    return .05m;
+                }
+                else if (_weight >= 2.218 && _weight <= 2.318
+                        && _size >= 17.86 && _size <= 17.96)
+                {
+                    return .1m;
+                }
+                else if (_weight >= 5.62 && _weight <= 5.72
+                        && _size >= 24.21 && _size <= 24.31)
+                {
+                    return .25m;
+                }
+                else if (_weight >= 11.29 && _weight <= 11.39
+                        && _size >= 30.56 && _size <= 30.66)
+                {
+                    return .5m;
+                }
+                else if (_weight >= 8.05 && _weight <= 8.15
+                        && _size >= 26.44 && _size <= 26.54)
+                {
+                    return 1m;
+                }
+                else
+                {
+                    return 0m;
+                }
+            }
+
+        }
+    }
+}
--- VendingMachines/VendingMachine.cs
+++ VendingMachines/VendingMachine.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace VendingMachines
 {
-    public enum Coin
-    {
-        None = 0,
-        Penny = 1,
-        Nickle = 5,
-        Dime = 10,
-        Quarter = 25
-    }
+    //public enum Coin
+    //{
+    //    None = 0,
+    //    Penny = 1,
+    //    Nickle = 5,
+    //    Dime = 10,
+    //    Quarter = 25
+    //}
 
     public enum Product
     {
@@ -21,25 +22,33 @@ namespace VendingMachines
 
     public class VendingMachine
     {
-        public const string CONST_THANKYOU = "THANK YOU";
-        public const string CONST_INSERTCOIN = "INSERT COIN";
-        public const string CONST_SOLDOUT = "SOLD OUT";
+        public const string THANKYOU = "THANK YOU";
+        public const string INSERTCOIN = "INSERT COIN";
+        public const string SOLDOUT = "SOLD OUT";
+        private static Coin _penny = new Coin();
+        private static Coin _nickel = new Coin(5.0, 21.21);
+        private static Coin _dime = new Coin(2.268, 17.91);
+        private static Coin _quarter = new Coin(5.67, 24.26);
+        private static Coin _halfDollar = new Coin(11.34, 30.61);
+        private static Coin _dollar = new Coin(8.1, 26.49);
 
-        private int _totalValue;
-        private int _coinReturnValue;
+        private decimal _totalValue;
+        //private int _coinReturnValue;
+        private List<Coin> _insertedCoins;
+        private List<Coin> _returnedCoins;
+
         private string _previousDisplay;
         private string _display;
         private Product _selectedProduct;
         private Dictionary<Product, int> _productStock;
 
-        //static void Main(string[] args)
-        //{}
-
         public VendingMachine()
         {
             _totalValue = 0;
-            _coinReturnValue = 0;
-            _display = CONST_INSERTCOIN;
+            //_coinReturnValue = 0;
+            _insertedCoins = new List<Coin>();
+            _returnedCoins = new List<Coin>();
+            _display = INSERTCOIN;
             _selectedProduct = Product.None;
 
             //Initialize product to have 5 each
@@ -47,9 +56,29 @@ namespace VendingMachines
             _productStock.Add(Product.Candy, 5);
             _productStock.Add(Product.Cola, 5);
             _productStock.Add(Product.Chips, 5);
+
+            //Initialize coin bank
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_nickel);
+            _insertedCoins.Add(_dime);
+            _insertedCoins.Add(_dime);
+            _insertedCoins.Add(_dime);
+            _insertedCoins.Add(_dime);
+            _insertedCoins.Add(_dime);
+            _insertedCoins.Add(_quarter);
+            _insertedCoins.Add(_quarter);
+            _insertedCoins.Add(_quarter);
+            _insertedCoins.Add(_quarter);
         }
 
-        public int totalValue
+        public decimal totalValue
         {
             get
             {
@@ -57,11 +86,29 @@ namespace VendingMachines
             }
         }
 
-        public int coinReturnValue
+        public decimal coinListValue(List<Coin> coins)
+        {
+            decimal total = 0;
+            foreach (Coin coin in coins)
+            {
+                total += coin.coinValue;
+            }
+            return total;
+        }
+
+        public decimal insertedTotalValue
         {
             get
             {
-                return _coinReturnValue;
+                return coinListValue(_insertedCoins);
+            }
+        }
+
+        public decimal returnTotalValue
+        {
+            get
+            {
+                return coinListValue(_returnedCoins);
             }
         }
 
@@ -81,27 +128,45 @@ namespace VendingMachines
             }
         }
 
+        bool IsValidCoin(Coin coin)
+        {
+            if (coin.coinValue.Equals(.05m) || coin.coinValue.Equals(.1m) || coin.coinValue.Equals(.25m))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool InsertCoin(double weight, double size)
+        {
+            Coin coin = new Coin(weight, size);
+            return InsertCoin(coin);
+        }
+
         public bool InsertCoin(Coin coin)
         {
             //Valid coins
-            if (coin.Equals(Coin.Nickle) || coin.Equals(Coin.Dime) || coin.Equals(Coin.Quarter))
+            if (IsValidCoin(coin))
             {
-                _totalValue += (int)coin;
-                _display = (_totalValue/100m).ToString("C2");
+                _insertedCoins.Add(coin);
+                _totalValue += coin.coinValue;
+                _display = (_totalValue).ToString("C2");
                 _previousDisplay = _display;
                 return true;
             }
             //Invalid coins
             else
             {
-                _coinReturnValue += (int)coin;
+                _returnedCoins.Add(coin);
                 return false;
             }
         }
 
         public bool SelectProduct(Product product)
         {
-            int price = (int)product;
+            decimal price = ((decimal)(int)product / 100);
             _selectedProduct = product;
 
             if (product != Product.None)
@@ -112,14 +177,14 @@ namespace VendingMachines
                     if (_productStock[product] > 0)
                     {
                         _totalValue = _totalValue - price;
-                        _display = CONST_THANKYOU;
+                        _display = THANKYOU;
                         _previousDisplay = _display;
                         _productStock[product] = _productStock[product] - 1;
                         return true;
                     }
                     else
                     {
-                        _display = CONST_SOLDOUT;
+                        _display = SOLDOUT;
                         _previousDisplay = _display;
                         return false;
                     }
@@ -127,7 +192,7 @@ namespace VendingMachines
                 //Not enough money for selected product
                 else
                 {
-                    _display = "PRICE " + (price / 100m).ToString("C2");
+                    _display = "PRICE " + price.ToString("C2");
                     _previousDisplay = _display;
                     return false;
                 }
@@ -141,24 +206,24 @@ namespace VendingMachines
         public void CheckDisplay()
         {
             //Previous display was the total value
-            if (_previousDisplay.Equals((_totalValue / 100m).ToString("C2")) ||
-                _previousDisplay.Equals(CONST_THANKYOU))
+            if (_previousDisplay.Equals(_totalValue.ToString("C2")) ||
+                _previousDisplay.Equals(THANKYOU))
             {
-                _display = CONST_INSERTCOIN;
+                _display = INSERTCOIN;
                 _previousDisplay = _display;
             }
             //Previous display was the price of the selected item
             else if (_previousDisplay.Equals("PRICE " + ((int)_selectedProduct / 100m).ToString("C2")) ||
-                _previousDisplay.Equals(CONST_SOLDOUT))
+                _previousDisplay.Equals(SOLDOUT))
             {
                 if(_totalValue > 0)
                 {
-                    _display = (_totalValue / 100m).ToString("C2");
+                    _display = _totalValue.ToString("C2");
                     _previousDisplay = _display;
                 }
                 else
                 {
-                    _display = CONST_INSERTCOIN;
+                    _display = INSERTCOIN;
                     _previousDisplay = _display;
                 }
             }
@@ -166,10 +231,30 @@ namespace VendingMachines
 
         public void ReturnCoins()
         {
-            _coinReturnValue = _totalValue;
+            _returnedCoins = MakeChange(ref _insertedCoins, _totalValue);
             _totalValue = 0;
-            _display = CONST_INSERTCOIN;
+            _display = INSERTCOIN;
             _previousDisplay = _display;
+        }
+
+        private List<Coin> MakeChange(ref List<Coin> coins, decimal value)
+        {
+            List<Coin> change = new List<Coin>();
+            if (value > 0 && coins.Count > 0)
+            {
+                while (value >= 0.05m)
+                {
+                    List<Coin> coinList = coins.Where(x => x.coinValue <= value).OrderByDescending(x => x.coinValue).ToList();
+                    var coin = coinList.First();
+                    if (coin.coinValue <= value)
+                    {
+                        change.Add(coin);
+                        coins.Remove(coin);
+                        value -= coin.coinValue;
+                    }
+                }
+            }
+            return change;
         }
     }
 }
